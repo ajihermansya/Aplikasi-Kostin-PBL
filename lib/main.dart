822b2f7b8df59c6d5e40e156768334d2d6035a7b
ajihermansya <ajihermansya14@gmail.com>
2022-12-02 14:27:58 +0700

    aji

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:kostin_p_b_l/widget/splash.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // return GetMaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   home: HomePage(),
+
+    // );
+    return FutureBuilder(
+      future: Future.delayed(Duration(seconds: 5)),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return SplashScreen();
+        } else {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "Bank Sampah Digital",
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+          );
+        }
+      },
+    );
+  }
 }
